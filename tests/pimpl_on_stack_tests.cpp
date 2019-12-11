7db6b074a8d10529a79b96d1491f0ec52711bdd5
Sergey Kuznetsov <kuznetsov.s@alager.ru>
2019-12-11 14:15:06 +0300

    Add test for constructor and destructor

--- tests/pimpl_on_stack_tests.cpp
+++ tests/pimpl_on_stack_tests.cpp
@@ -2,17 +2,47 @@
 
 #include "pimpl_on_stack/pimpl_on_stack.h"
 
-TEST(Pimpl, First) {
+TEST(Pimpl, Constructor_destructor) {
+  bool constructor_used = false;
+  bool destructor_used = false;
+
   class C {
    public:
-    C() { std::cout << "C constructor" << std::endl;}
-    ~C() { std::cout << "C destructor" << std::endl;}
-    int i = 0;
+    C(bool* constructor_used, bool* destructor_used)
+        : destructor_used_(destructor_used) {
+      *constructor_used = true;
+    }
+    ~C() { *destructor_used_ = true; }
+
+   private:
+    bool* destructor_used_;
   };
-  pimpl_on_stack::Pimpl<C, 4, 4> c;
+
+  { pimpl_on_stack::Pimpl<C, 8, 8> c(&constructor_used, &destructor_used); }
+  EXPECT_TRUE(constructor_used);
+  EXPECT_TRUE(destructor_used);
 }
 
-int main(int argc, char *argv[]) {
+TEST(Pimpl, Default_constructor) {
+  constexpr int kValue = 123;
+
+  struct S {
+    int i = kValue;
+  };
+
+  pimpl_on_stack::Pimpl<S, 4, 4> pimpl;
+  EXPECT_EQ(pimpl->i, kValue);
+}
+
+TEST(Pimpl, Move_constructor) {}
+
+TEST(Pimpl, Operator_eq) {}
+
+TEST(Pimpl, Operator_derefference) {}
+
+TEST(Pimpl, Reset) {}
+
+int main(int argc, char* argv[]) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
